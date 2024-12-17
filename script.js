3d84e2d55bee11042c785afe781ebf8691bcb087
Jose2003-web <jose.pena@linktic.com>
2024-12-17 12:20:39 -0500

    se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,10 @@
+var form = document.getElementById("myform")
+
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+    var search = document.getElementById("search").value
+
+    alert(search)
+})
